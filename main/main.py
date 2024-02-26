99a9cdeb4e31c1505bf5c666d39e2780e0828895
SaiyedRushan <rushan52@gmail.com>
2024-02-26 18:07:52 -0500

    define api endpoints for main app

--- main/main.py
+++ main/main.py
@@ -1,9 +1,11 @@
 from dataclasses import dataclass
-from flask import Flask
+from flask import Flask, jsonify, abort
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import UniqueConstraint
 from flask_migrate import Migrate
+import requests
+from producer import publish
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:root@db/main"
@@ -31,6 +33,27 @@ class ProductUser(db.Model):
     product_id = db.Column(db.Integer)
 
     UniqueConstraint("user_id", "product_id", name="user_product_unique")
+
+
+@app.route("/api/products")
+def index():
+    return jsonify(Product.query.all())
+
+
+@app.route("/api/products/<int:id>/like", methods=["POST"])
+def like(id):
+    req = requests.get("http://docker.for.windows.localhost:8000/api/user")
+    json = req.json()
+
+    try:
+        productUser = ProductUser(user_id=json["id"], product_id=id)
+        db.session.add(productUser)
+        db.session.commit()
+        publish("product_liked", id)
+    except:
+        abort(400, "You already liked this product")
+
+    return jsonify({"message": "success"})
 
 
 if __name__ == "__main__":
